19520d10db6445264eea41a63cb24619ec367d0a
Surekha1919 <surekha.dasari63@gmail.com>
2020-12-17 15:40:49 +0530

    tailwind is configured

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ngt app is running!');
+    expect(page.getTitleText()).toEqual('connectocode app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngt'`, () => {
+  it(`should have as title 'connectocode'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngt');
+    expect(app.title).toEqual('connectocode');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngt app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('connectocode app is running!');
   });
 });
