78d1d655abebea73970636e530390e934d408e4b
sricharan <sricharan.donkada@gmail.com>
2020-12-22 12:16:07 +0530

    card component template is created: dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { C2cCardModule } from './ngt-components/card/card.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    C2cCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
