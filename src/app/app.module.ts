d0528ad7f2a4e071c47a32fac4b4291093c95e41
tulasi1356 <botchatulasi1356@gmail.com>
2021-01-15 17:55:33 +0530

    ripple effect in buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { C2cCardModule } from './ngt-components/card/card.module';
