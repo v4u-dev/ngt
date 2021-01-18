7a89baa671a36761c78638c96b2970bbfc15e3a1
tulasi1356 <botchatulasi1356@gmail.com>
2021-01-18 12:39:23 +0530

    expansion animation is completed

--- src/app/ngt-components/expansion-panel/expansion-panel.component.ts
+++ src/app/ngt-components/expansion-panel/expansion-panel.component.ts
@@ -1,11 +1,30 @@
 import { ChangeDetectionStrategy, Component, Directive, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
+import { animate, state, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'c2c-expansion-panel',
   templateUrl: './expansion-panel.component.html',
-  styleUrls: ['./expansion-panel.component.scss']
+  styleUrls: ['./expansion-panel.component.scss'],
+  animations : [
+    trigger('animationShowHide', [
+      state('close', style({ height: '0px', overflow: 'hidden' })),
+      state('open', style({ height: '*',overflow: 'hidden'})),
+      transition('open <=> close', animate('900ms ease-in-out')),
+    ]),
+    trigger('animationRotate', [
+      state('close', style({ transform: 'rotate(0)' })),
+      state('open', style({ transform: 'rotate(-180deg)' })),
+      transition('open <=> close', animate('900ms ease-in-out')),
+    ]),
+  ],
 })
 export class ExpansionPanelComponent implements OnInit {
+  sStatus = 'open';
+
+  onSwitch(){
+    console.log('rfffffffffffffffffff')
+    this.sStatus = this.sStatus === 'close' ? 'open' : 'close';
+  }
 
   constructor() { }
 
