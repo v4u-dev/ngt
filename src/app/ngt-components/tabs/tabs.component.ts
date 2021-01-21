8a7a98c77d2fc3bd473a6ad94f5c6ae43a8b39d2
osarath265 <osarath265@gmail.com>
2021-01-21 13:35:21 +0530

    style parameters can be passed to tabs component now

--- src/app/ngt-components/tabs/tabs.component.ts
+++ src/app/ngt-components/tabs/tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
 import {TabComponent} from '../tab/tab.component';
 
 @Component({
@@ -12,11 +12,18 @@ import {TabComponent} from '../tab/tab.component';
 export class TabsComponent implements OnInit {
 
     tabs: TabComponent[] = [];
+
+    @Input() bgColor: string;
+    @Input() color: string;
+    @Input() fontsize: string;
+    @Input() fontweight:string;
     
+
+
+
   selectTab(tab: TabComponent) {
     this.tabs.forEach((tab) => {
       tab.active = false;
-    
     });
     tab.active = true;
   }
