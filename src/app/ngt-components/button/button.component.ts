1580c47618295e9e14d3db095bf4421695aaa5cc
msriramvarma <msriramvarma01@gmail.com>
2021-01-21 13:08:17 +0530

    Button Parameters Added

--- src/app/ngt-components/button/button.component.ts
+++ src/app/ngt-components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ngt-button',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
-  constructor() { }
+  @Input() color: String;
+
+  constructor() {
+    console.log(this.color);
+   }
 
   ngOnInit(): void {
   }
