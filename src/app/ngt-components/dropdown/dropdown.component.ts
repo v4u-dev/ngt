bbb857985f24dc1e103b2222259de5d9aa9c5660
bhavyasssweety <sankubhavyasri@gmail.com>
2021-02-06 22:25:36 +0530

    oncecheck

--- src/app/ngt-components/dropdown/dropdown.component.ts
+++ src/app/ngt-components/dropdown/dropdown.component.ts
@@ -1,76 +1,34 @@
-import { Directive } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
-import { SlideInOutAnimation } from '../../animate';
-@Component({
-  selector: 'ngt-dropdown',
-  templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.scss'],
-  animations: [SlideInOutAnimation]
+// import { Directive } from '@angular/core';
+import { Component, Input, OnInit, Directive, ElementRef } from '@angular/core';
+
+// import { Component, OnInit } from '@angular/core';
+// import { SlideInOutAnimation } from '../../animate';
+// @Component({
+//   selector: 'ngt-dropdown',
+//   templateUrl: './dropdown.component.html',
+//   styleUrls: ['./dropdown.component.scss'],
+//   animations: [SlideInOutAnimation]
+// })
+// export class DropdownComponent implements OnInit {
+
+//   constructor() { }
+//   ngOnInit(): void {
+//     throw new Error('Method not implemented.');
+//   }
+// }
+@Directive({
+  selector: '[ngt-select]',
 })
-export class DropdownComponent implements OnInit {
+export class NgtSelect {
+  constructor(el: ElementRef){
+    // el.nativeElement.classList.add('p-10');
+    // el.nativeElement.classList.add('bg-green');
+    // //default stylings applied for a button
+    // el.nativeElement.classList.add('py-3');//padding-top and bottom
+    // el.nativeElement.classList.add('px-4'); //padding-left and right
+    // el.nativeElement.classList.add('bg-gray-100'); //background-color
+    // el.nativeElement.classList.add('rounded'); //border-radius
+    // el.nativeElement.classList.add('border-gray-200'); //border-color
 
-  constructor() { }
-  public dropDownarr = ['Profile', 'Name', 'Address']
-  public dropDownarratProfile = ['My Profile', 'Settings', 'Buddies', 'Clans', 'Logout']
-  public dropDownarratPost = ['New Posts', 'Top Posts', 'Hot Posts']
-  animationState = 'out';
-  animationfornewdropdown = 'out'
-  animationformahishmathidropdown = 'out'
-  animationforlanguagedropdown = 'out'
-  animationforpostdropdown = 'out'
-  animationforprofiledropdown = 'out'
-  toggleShowDiv(divName: string) {
-    if (divName == 'Python Programming') {
-      this.animationState = this.animationState === 'out' ? 'in' : 'out';
-    }
-    if (divName == 'New') {
-      this.animationfornewdropdown = this.animationfornewdropdown === 'out' ? 'in' : 'out';
-    }
-    if (divName == 'Mahishmati') {
-      this.animationformahishmathidropdown = this.animationformahishmathidropdown === 'out' ? 'in' : 'out';
-    }
-    if (divName == 'Language') {
-      this.animationforlanguagedropdown = this.animationforlanguagedropdown === 'out' ? 'in' : 'out';
-    }
-    if (divName == 'Post') {
-      this.animationforpostdropdown = this.animationforpostdropdown === 'out' ? 'in' : 'out';
-    }
-    if (divName == 'Profile') {
-      this.animationforprofiledropdown = this.animationforprofiledropdown === 'out' ? 'in' : 'out';
-    }
   }
-  ngOnInit(): void {
-  }
-
 }
-
-
-@Directive({
-  selector: 'ngt-dropdown-programmig',
-})
-export class ngtdropdownprogrammig { }
-
-@Directive({
-  selector: 'ngt-dropdown-New',
-})
-export class ngtdropdownNew { }
-
-@Directive({
-  selector: 'ngt-dropdown-clans',
-})
-export class ngtdropdownclans { }
-
-@Directive({
-  selector: 'ngt-dropdown-languages',
-})
-export class ngtdropdownlanguages { }
-
-@Directive({
-  selector: 'ngt-dropdown-posts',
-})
-export class ngtdropdownposts { }
-
-@Directive({
-  selector: 'ngt-dropdown-profile',
-})
-export class ngtdropdownprofile { }
